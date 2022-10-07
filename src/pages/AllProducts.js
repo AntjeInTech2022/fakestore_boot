8716d22e2868423f9f10c91888fb2dd96c3d6883
AntjeInTech2022 <as.wit2022@startsteps.de>
2022-10-07 16:41:50 +0200

    added tooltip feature

--- src/pages/AllProducts.js
+++ src/pages/AllProducts.js
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom'
 import { BsBookmark, BsCartPlus  } from "react-icons/bs";
 import '../App.css';
 import Banner from '../components/Banner';
-//import Container from 'react-bootstrap/Container';
+
 
 
 
@@ -27,19 +27,26 @@ console.log('response', response)
     })();
   },[]);
 
-  // Tooltip
-  const renderTooltip = (props) => (
-    <Tooltip id="button-tooltip" {...props}>
-      Simple tooltip
-    </Tooltip>
-      );
-
+  // Button routing
     let navigate = useNavigate();
+
+  // Tooltips
+    const renderTooltipBookmark = (props) => (
+      <Tooltip id="button-tooltip1" {...props}>
+        Save item to favorites
+      </Tooltip>
+    );
+
+    const renderTooltipCart = (props) => (
+      <Tooltip id="button-tooltip2" {...props}>
+        Add to cart
+      </Tooltip>
+    );
 
 
   return (
   
-    <Row xs={2} md={5} className="g-4">
+    <Row xs={1} md={4} className="g-4">
       {/* 'products &&' is a condition meaning: do if products  != null */}
       {products && products.map((product, i) => (
         <Col key={product.id} >
@@ -55,13 +62,21 @@ console.log('response', response)
               <Button onClick={()=>navigate(`/product/${product.id}`)} variant="danger">Show more</Button>
               
               <OverlayTrigger
-                placement="right"
-                delay={{ show: 250, hide: 400 }}
-                 overlay={renderTooltip}>
-                  <BsBookmark className='bookmark'/>    
+                placement="top"
+                // delay={{ show: 250, hide: 400 }}
+                overlay={renderTooltipBookmark}>
+                <Button variant="successwhite"><BsBookmark className='bookmark'/></Button>
+              </OverlayTrigger>
+
+                <OverlayTrigger
+                placement="top"
+                // delay={{ show: 250, hide: 400 }}
+                overlay={renderTooltipCart}>
+                <Button variant="white"><BsCartPlus className="BsCartPlus"/></Button>
               </OverlayTrigger>
               
-              <BsCartPlus className="BsCartPlus"/>
+              
+              
             {/* <Link to={`/product/${product.id}`}>Show /product/${product.id}</Link>  */}
             </Card.Body>
             {/* <Card.Footer>
