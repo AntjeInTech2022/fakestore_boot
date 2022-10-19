11abb73daebe53eebf777c6c3d096661da512050
AntjeInTech2022 <as.wit2022@startsteps.de>
2022-10-19 14:16:09 +0200

    readWishlist.js done for today

--- src/components/readWishlist.js
+++ src/components/readWishlist.js
@@ -1,23 +1,24 @@
 import {React, useContext}  from 'react'
-import { collection, query, where, getDocs, collectionGroup } from "firebase/firestore";
+import { doc, collection, query, where, getDocs, collectionGroup, getDoc } from "firebase/firestore";
 import {db} from "../context/firebase";
 import { AuthContext } from "../context/authContext";
+import { Col, Row} from 'react-bootstrap';
 
 function ReadWishlist({product}) {
 
-const {user} = useContext(AuthContext)
-   
-// Get all documents in a collection
-const q = query(collection(db, "users", user.uid, 'wishlist'), where(user.uid, "==", user.uid));
+    const {user} = useContext(AuthContext)
+
+// Get all documents in a collection:
+const q = query(collection(db, "users", user.uid, 'wishlist'), where(user.uid, "==", user.id));
 
 const querySnapshot = getDocs(q);
 querySnapshot.forEach((doc) => {
-  // doc.data() is never undefined for query doc snapshots
   console.log(doc.id, " => ", doc.data());
 });
 
-// Collection Group
+// Collection Group: requires an index
 // https://firebase.google.com/docs/firestore/query-data/queries?authuser=0&hl=en#collection-group-query
+
 // const items = query(collectionGroup(db, 'wishlist'), where(user.uid, '==',  user.uid));
 // const querySnapshot = getDocs(items);
 // querySnapshot.forEach((doc) => {
@@ -25,12 +26,17 @@ querySnapshot.forEach((doc) => {
 // });
 
 
-  return (
-    <div> 
-        {/* <p>Product id: {items && items.id}</p>
-        <p>Product title: {items && items.title}</p> */}
-    </div>
-  )
+
+//   return (
+//     <Row>
+//   {items && items.map((item, i) => (
+//           <Col key={item.id}>
+//           <p>Product id: {item.id}</p>
+//           <p>Product title: {item.title}</p>
+//           </Col>
+//         ))}
+//     </Row> 
+//   )
 }
 
-export default ReadWishlist
+export default ReadWishlist;
