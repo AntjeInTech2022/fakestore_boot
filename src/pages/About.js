418236ed718982ac72ed7f7d5e9b97bece98e33b
AntjeInTech2022 <as.wit2022@startsteps.de>
2022-10-24 10:25:55 +0200

    about.js done

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,46 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 
 function About() {
   return (
-    <div><h3>About this project</h3>
-    <p>This project was part of the Code Academy Berlin Full-Stack Web Developer Bootcamp. The project consisted in building a free-of-choice Single Page Application with React JS using Firebase to create a login feature and to store information into a database.
-- API used: https://fakestoreapi.com/
-- CSS framework used: Bootstrap React</p>
+<div>
+<h3>About this project</h3>
+<p>This project was part of the Code Academy Berlin Full-Stack Web Developer Bootcamp. The project consisted in building a free-of-choice Single Page Application with React JS using Firebase to create a login feature and to store information into a database.</p>
+<ul>
+  <li>API used: https://fakestoreapi.com/</li>
+  <li>CSS framework used: Bootstrap React</li>
+</ul>
+<h3>Learning goals:</h3>
+<ul>
+<li>Create and configure a React project</li>
+<li>Understand the component hierarchy in React</li>
+<li>Create a navigation between pages using the React Router</li>
+<li>Use of global store (Context Hook)</li>
+<li>Use Firebase to create a registration / login feature by email and password</li>
+<li>Optional: signin using Google OAuth</li>
+<li>Use Firebase Cloud Firestore functionality to create a chat room feature or something alike</li>
+</ul>
+<h3>Requirements:</h3>
+<ul>
+<li>API providing a list and detailed endpoints</li>
+<li>Design mobile first</li>
+<li>Single Page Application (using a modern JS Framework)</li>
+</ul>
+<h3>Additional features implemented:</h3>
+<ul>
+<li>Protected Routes</li>
+<li>Save favourites to a Wishlist (by writing, reading and deleting documents in Firestore)</li>
+</ul>
+
+
+
+</div>
+
     
-    </div>
+
+    
+
   )
 }
 
-export default About
+export default About;
